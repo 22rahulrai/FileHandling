163bfd43a9907ec68dd85b9f031e2560f2657f6e
Rahul Rai <rairayrahul22@gmail.com>
2025-03-27 19:34:37 +0530

    Conatain Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+
+namespace File_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Basicofiles.method1();
+            //File_methods.filemethods();
+            //Stream_reader_writer.method4();
+            Csvfilehandling.Method();
+            //Jsonfilehandling.Method1();
+            
+        }
+    }
+}
+
